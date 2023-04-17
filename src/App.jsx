ab0a6923493383ee9e7148a6e603a1bd594ecbf5
Dallana Cardoza Martinez <cardozadallana16@gmail.com>
2023-04-17 10:05:50 -0600

    se da la opcion de marcar la casilla de seleccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react"
+
 function App() {
 
+  const [itemChecked, setitemChecked] = useState({
+    first: false,
+    second: false,
+    third: false,
+
+  })
+
+  const handleItemChecked = (e) => {
+   /* if (itemChecked[e.target.name]){
+set)ItemChecked({
+  ...itemChecked,
+  [e.target.name]: false, 
+});
+    }else{
+      setItemChecked
+    }
+  
+  }*/
+
+  setitemChecked({
+    ...itemChecked,
+    [e.target.name]:!itemChecked[e.target.name]
+  })
+  }
   return (
     <div className="container text-center"> 
     <div className="row"> 
@@ -17,10 +43,33 @@ function App() {
 
     <div className="row">  
     <div className="col"> 
-    <input type="checkbox"/>
+    <input 
+    type="checkbox" 
+    checked={itemChecked.first}
+    name="first"
+    onClick={(e) => handleItemChecked(e)}
+    />
      </div>
-    <div className="col-2 text-start "> 1 Kg </div>
-    <div className="col-5 col-md-6 text-start"> Tortillas </div>
+    <div className="col-2 text-start "> 
+    {
+     // itemChecked.first && <del>1 Kg </del>
+    }
+
+   {
+      //!itemChecked.first && "1 Kg "
+    }
+    {
+      itemChecked.first ? <del>1 Kg </del> : "1 kg"
+    }
+      
+    </div>
+
+    <div 
+    className="col-5 col-md-6 text-start" 
+    style={{textDecoration: itemChecked.first && "line-through"}}
+    >
+       Tortillas 
+    </div>
     <div className="col-4 col-md-3 btn-group btn-group-sm" role="group"> 
     <button type="button" className="btn btn-outline-primary">
       <i className="bi bi-pencil-square"></i>
